ff5269aa2d5d375be0b269c3968576739352c838
paulaapark <99138656+paulaapark@users.noreply.github.com>
2022-11-06 15:43:53 -0500

    modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/styles.css
    Set up the assignment, modified the above files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'greeting-app';
+  title = 'Angular';
+  studentName = 'Paula';
 }
